d682bf28179f3797be30b59d0e9417424610bb96
rahees <raheez1995india@gmail.com>
2025-04-22 14:10:20 +0530

    about us page and common page banner

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Index from "../components/about";
+
+const About = () => {
+  return ( 
+    <>
+      <Index/>
+    </>
+   );
+}
+ 
+export default About;
---
+++ app/components/about/data.tsx
@@ -0,0 +1,8 @@
+export const aboutMainDesc = {
+  title: "Our Company",
+  subTitle: "Safe Tech Precast ",
+  subTitleSpan: "Building Manufacturing",
+  desc: "At Safe Tech, we recognize the importance of customized solutions for various project types. Whether it's commercial spaces, residential developments, industrial facilities, accommodations, or boundary walls, we are committed to meeting the specific requirements of each project. Our team takes pride in contributing to a diverse range of developments, ensuring that every construction is built to last and enhances the spaces where people live, work, and thrive. ",
+  desc2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ctetur adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+  mainImg: "/assets/img/about/about-main.jpg",
+}
