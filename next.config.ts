2bf271a6c9803ff13e13dbcf03aae29192424496
rahees <raheez1995india@gmail.com>
2025-05-01 13:01:59 +0530

    versal image issue fix

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatar.iran.liara.run",
-        port: "",
-        pathname: "/public/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dl.dropboxusercontent.com",
-        port: "",
-        pathname: "/scl/**",
-      },
-    ],
-    dangerouslyAllowSVG: true,
     unoptimized: true,
   },
 };
