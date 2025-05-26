72f4461412714405980c51f203d94d6c1238b3ac
rahees <raheez1995india@gmail.com>
2025-05-26 17:08:25 +0530

    conflict fix

---
+++ app/(user)/precast-pre-stressed/[element]/page.tsx
@@ -0,0 +1,10 @@
+import Index from '@/app/components/precast-pre-stressed-element'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <Index/>
+    </>
+  )
+}
