f3a73016a37e8725b0ed4b236e2ccae37a99ead5
Nived <nived@globalsurf.ae>
2025-06-05 11:44:49 +0530

    fix:downloads not saving

--- app/api/admin/resources/meta/route.ts
+++ app/api/admin/resources/meta/route.ts
@@ -11,20 +11,14 @@ export async function POST(req:NextRequest) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
         const { banner, bannerAlt, pageTitle, metaTitle, metaDescription } = await req.json();
-        const resource = await Resource.create({
-            banner,
-            bannerAlt,
-            pageTitle,
-            metaTitle,
-            metaDescription,
-        });
+        const resource = await Resource.findOne({});
         if(resource){
-            // resource.banner = banner;
-            // resource.bannerAlt = bannerAlt;
-            // resource.pageTitle = pageTitle;
-            // resource.metaTitle = metaTitle;
-            // resource.metaDescription = metaDescription;
-            // await resource.save();
+            resource.banner = banner;
+            resource.bannerAlt = bannerAlt;
+            resource.pageTitle = pageTitle;
+            resource.metaTitle = metaTitle;
+            resource.metaDescription = metaDescription;
+            await resource.save();
             return NextResponse.json({ message: "meta section updated successfully" }, { status: 200 });
         }else{
             return NextResponse.json({ message: "Error updating meta section" }, { status: 500 });
